7de3d5f7e2fbd44436f1d1686842923aa6a24290
kunal445 <73367589+kunal445@users.noreply.github.com>
2022-04-27 18:46:40 -0400

    added proxy api call

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // using this proxy server because of cors error with the api
+  
+  //  const proxiedUrl = 'https://www.mmobomb.com/api1/games';
+
+  //           const url = new URL('http://proxy.hackeryou.com');
+  //           url.search = new URLSearchParams({
+  //               reqUrl: proxiedUrl,
+  //           });
+
+  //           fetch(url)
+  //               .then(response => response.json())
+  //               .then(data => {
+  //                   console.log(data);
+  //               });
+
   return (
     <div className="App">
       <header className="App-header">
