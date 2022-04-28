bd776a5bab98757cdf457d4668b88d8e8398b887
kunal445 <73367589+kunal445@users.noreply.github.com>
2022-04-28 16:58:27 -0400

    fixed an issue where the images were breaking out of their container and created the form component

---
+++ src/componenents/Form.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Form = () => {
+    return (
+        <div>
+            <form action="">
+                <select id='platform' name='platform'>Selected your platform</select>
+                <option value='PC'>PC</option>
+            </form>
+        </div>
+    )
+}
+
+export default Form
